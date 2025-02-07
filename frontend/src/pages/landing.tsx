dab24bd1b4662d3efc2561596e55ed00aab9ae16
afadel151 <af8356207@gmail.com>
2025-02-07 18:36:31 +0100

    changed spline scene

--- frontend/src/pages/landing.tsx
+++ frontend/src/pages/landing.tsx
@@ -9,7 +9,7 @@ const LandingPage = () => {
         <div className="h-full w-full"> 
           <p className="text-8xl text-white text-center ">Astronomy Club</p>
           <p className="text-3xl text-white text-center"> EMP</p>
-          <Spline className="h-screen absolute top-0 left-0 -z-10 w-screen" scene="https://prod.spline.design/MTbkrFuDx9tPJaWp/scene.splinecode" />
+          <Spline className="h-screen absolute top-0 left-0 -z-10 w-screen" scene="https://prod.spline.design/v4Si4zH9B-UDndwk/scene.splinecode" />
         </div>
         
     </>
